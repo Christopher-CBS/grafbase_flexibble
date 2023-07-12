3c8a711365bc3e536e853c9617384cc8c0654dcf
Christopher-CBS <christopherbadibanga2014@gmail.com>
2023-07-12 15:16:13 +0200

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,96 @@
+export const NavLinks = [
+    { href: '/', key: 'Inspiration', text: 'Inspiration' },
+    { href: '/', key: 'Find Projects', text: 'Trouver des projets' },
+    { href: '/', key: 'Learn Development', text: 'Apprendre à développer' },
+    { href: '/', key: 'Career Advancement', text: 'Progression de carrière' },
+    { href: '/', key: 'Hire Developers', text: 'Recruter des développeurs' }
+  ];
+  
+  export const categoryFilters = [
+    "Frontend",
+    "Backend",
+    "Full-Stack",
+    "Mobile",
+    "UI/UX",
+    "Game Dev",
+    "DevOps",
+    "Data Science",
+    "Machine Learning",
+    "Cybersecurity",
+    "Blockchain",
+    "E-commerce",
+    "Chatbots"
+  ]
+  
+  export const footerLinks = [
+    {
+      title: 'Pour les développeurs',
+      links: [
+        'Go Pro!',
+        'Explorer les travaux de développement',
+        'Development blog',
+        'Code podcast',
+        'Open-source projects',
+        'Refer a Friend',
+        'Code of conduct',
+      ],
+    },
+    {
+      title: 'Recruter des développeurs',
+      links: [
+        'Post a job opening',
+        'Post a freelance project',
+        'Search for developers',
+      ],
+    },
+    {
+      title: 'Les marques',
+      links: [
+        'Advertise with us',
+      ],
+    },
+    {
+      title: 'Entreprise',
+      links: [
+        'About',
+        'Careers',
+        'Support',
+        'Media kit',
+        'Testimonials',
+        'API',
+        'Terms of service',
+        'Privacy policy',
+        'Cookie policy',
+      ],
+    },
+    {
+      title: 'Répertoires',
+      links: [
+        'Development jobs',
+        'Developers for hire',
+        'Freelance developers for hire',
+        'Tags',
+        'Places',
+      ],
+    },
+    {
+      title: 'Actifs de développement',
+      links: [
+        'Code Marketplace',
+        'GitHub Marketplace',
+        'NPM Registry',
+        'Packagephobia',
+      ],
+    },
+    {
+      title: 'Ressources de développement',
+      links: [
+        'Freelancing',
+        'Development Hiring',
+        'Development Portfolio',
+        'Development Education',
+        'Creative Process',
+        'Development Industry Trends',
+      ],
+    },
+  ];
